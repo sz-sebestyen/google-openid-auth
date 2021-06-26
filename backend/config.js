6c08fb9cea4986eff34728a1652c767975b52507
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-06-26 17:24:25 +0200

    Post code to google

--- backend/config.js
+++ backend/config.js
@@ -6,4 +6,6 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRES_IN: parseInt(process.env.JWT_EXPIRES_IN),
   FRONTEND_HOST: process.env.FRONTEND_HOST,
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,9 +1,35 @@
 const AuthEntity = require("../models/AuthEntity");
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET, JWT_EXPIRES_IN } = require("../config");
+const {
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  CLIENT_SECRET,
+  CLIENT_ID,
+} = require("../config");
+const fetch = require("node-fetch");
 
 module.exports = async function loginUser(req, res, next) {
-  console.log(req.body.code);
+  const code = req.body.code;
+  console.log(code);
+
+  const postRes = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      redirect_uri: "http://localhost:3000/login",
+      grant_type: "authorization_code",
+    }),
+  });
+
+  const json = await postRes.json();
+
+  console.log("post resp: ", json);
+
   res.json({ message: "not implemented" });
 };
 
